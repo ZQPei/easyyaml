6c103e360e6695ce8f771b6fc43f0949182c3abf
ZQPei <dfzspzq@163.com>
2020-10-13 21:30:36 +0800

    [EasyYaml] add functions

--- easyyaml/__init__.py
+++ easyyaml/__init__.py
@@ -1,7 +1,39 @@
+import os
 import yaml
+from easydict import EasyDict as edict
 
-class EasyYaml(object):
-    ...
+__version__ = '0.0.1'
+
+_yaml_ext = ('.yml', '.yaml')
+
+class EasyYaml(edict, list):
+    def __init__(self, config=None):
+        assert isinstance(config, (str, dict)), "Value Type Error"
+
+        if isinstance(config, str):
+            assert os.path.isfile(config) and os.path.splitext(config)[-1] in _yaml_ext
+            with open(config, 'r') as foo:
+                _dict = yaml.load(foo)
+                if 
+
+        elif isinstance(config, edict):
+            _dict = config
+        else:
+            _dict = config
+
+    
+
+
+def load(): ...
+
+
+def dump(): ...
+
+
+def update(): ...
+
+
+def pop(): ...
 
 
 if __name__ == '__main__':
